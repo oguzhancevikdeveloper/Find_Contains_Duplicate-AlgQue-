63fe662bec2d1fb9db92786bf01178d86c1c18a6
oguzhan.cevik <oguzhancevik.developer@gmail.com>
2022-07-20 23:30:23 +0300

    Add project files.

---
+++ Questions/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Questions
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      List<int> numbers = new List<int>();
+      numbers.Add(1);
+      numbers.Add(2);
+      numbers.Add(3);
+      numbers.Add(1);
+
+      foreach (var item in numbers)
+      {
+        foreach (var itemm in numbers)
+        {
+          if(item == itemm)
+          {
+            Console.WriteLine("Duplicate");
+          }
+          else
+          {
+            Console.WriteLine("No Duplicate!!!");
+          }
+        }
+
+      }
+      Console.ReadLine();
+
+    }
+  }
+}
